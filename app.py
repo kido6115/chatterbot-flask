227f8fb668287fa1f6b20407c6fbdc1280ae8238
sungyeh <sungyeh@tist.com.tw>
2021-07-21 16:13:47 +0800

    t

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request
 from chatterbot import ChatBot
 
 app = Flask(__name__)
-chatbot = ChatBot("ChineseChatBot")
+chatbot = ChatBot("ChineseChatBot",
+    storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+    database_uri='mongodb+srv://admin:1111@cluster0.nwewq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE')
 
 
 @app.route('/<faq>')
